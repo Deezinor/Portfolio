97d01fed9e9e4f1a6d27ff6ef26ebb5f4224345e
Jake Boulton <jboultonwork@gmail.com>
2025-11-12 16:05:35 +0000

    feat: Model Viewer added to Savernake
    - knife model now integrated into savernake media gallery

--- portfolio_JakeBoulton/src/pages/projects/3D Art/Savernake.tsx
+++ portfolio_JakeBoulton/src/pages/projects/3D Art/Savernake.tsx
@@ -1,32 +1,43 @@
-import React from "react";
+// import React from "react";
 import ProjectTemplate from "../../../components/ProjectTemplate";
+import type { MediaItem } from "../../../types/media";
+
 // import { Link } from "react-router-dom";
 
-const media: { type: "image" | "video" | "iframe"; src: string }[] = [
+// ===== Asset Imports =====
+import KnifeUrl from "../../../../public/Assets/Models/202504_SpectraTomAikens_V01.glb?url";
+
+const media: MediaItem[] = [
   {
     type: "image",
     src: "./Assets/Images/Projects/Savernake/202504_3KnifeRender_V01.jpg",
   },
+  {
+    type: "model",
+    src: KnifeUrl,
+    envPreset: "city",
+    autoRotate: false,
+    tailwindHeightClass: "w-full h-[60vh]",
+  },
 ];
 
-const SavernakeKnife: React.FC = () => {
-  const data = {
-    title: "Savernake Knife",
-    projectLogo: "./Assets/Images/Projects/Savernake/Logo_whiteBg.jpg",
-    projectDescription: (
-      <>
-        As part of a targeted job application, I selected a signature piece from
-        Savernake Knives and recreated it in 3D using Blender. The project
-        focused on replicating the brand’s distinct form language, material
-        qualities, and attention to detail. I then rendered the knife in a
-        studio-style environment to highlight craftsmanship and presentation,
-        aiming to demonstrate both technical proficiency and an understanding of
-        the brand’s visual identity.
-      </>
-    ),
-    media: media,
-  };
-  return <ProjectTemplate {...data} />;
-};
-
-export default SavernakeKnife;
+export default function Savernake() {
+  return (
+    <ProjectTemplate
+      title="Savernake Knife"
+      projectLogo="./Assets/Images/Projects/Savernake/Logo_whiteBg.jpg"
+      projectDescription={
+        <p>
+          As part of a targeted job application, I selected a signature piece
+          from Savernake Knives and recreated it in 3D using Blender. The
+          project focused on replicating the brand’s distinct form language,
+          material qualities, and attention to detail. I then rendered the knife
+          in a studio-style environment to highlight craftsmanship and
+          presentation, aiming to demonstrate both technical proficiency and an
+          understanding of the brand’s visual identity.
+        </p>
+      }
+      media={media}
+    />
+  );
+}
